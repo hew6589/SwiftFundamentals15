34bb2796100698785aa5a816df465a945589adf0
hew6589 <wrheasian9189@hotmail.com>
2024-10-26 21:28:53 -0400

    Update Contents.swift

--- student_Xcode15/Unit1/Lesson1_4/Lab_Operators.playground/Pages/4. App Exercise - Counting.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit1/Lesson1_4/Lab_Operators.playground/Pages/4. App Exercise - Counting.xcplaygroundpage/Contents.swift
@@ -5,13 +5,16 @@
  
  The most basic feature of your fitness tracking app is counting steps. Create a variable `steps` and set it equal to 0. Then increment its value by 1 to simulate a user taking a step.
  */
-
+var steps = 0
+steps +=1 // 1
 
 /*:
  In addition to tracking steps, your fitness tracking app tracks distance traveled. Create a variable `distance` of type `Double` and set it equal to 50. This will represent the user having traveled 50 feet.
- 
+var distance: Double = 50
+
  You decide, however, to display the distance in meters. 1 meter is approximately equal to 3 feet. Use a compound assignment operator to convert `distance` to meters. Print the result.
  */
+distance /=3 // 15 m 
 
 
 /*:

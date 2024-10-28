1c20b63ae67bee0fe0b316ad3cce217bd16c6d9e
hew6589 <wrheasian9189@hotmail.com>
2024-10-27 21:01:51 -0400

    Update Contents.swift

--- student_Xcode15/Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/7. App Exercise - Heart Rate Zones.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/7. App Exercise - Heart Rate Zones.xcplaygroundpage/Contents.swift
@@ -12,10 +12,25 @@
 - 141-160: "You are in the Moderate zone. Activity in this zone helps improve aerobic fitness."
 - 161-180: "You are in the Hard zone. Activity in this zone increases maximum performance capacity for shorter sessions."
 - 181-200: "You are in the Maximum zone. Activity in this zone helps fit athletes develop speed."
+
+switch currentHR {
+case 100...120:
+print(You are in the Very Light zone. Activity in this zone helps with recovery.)
+case 121...140:
+print(You are in the Light zone. Activity in this zone helps improve basice endurance and fat burning.)
+case 141...160:
+print(You are in the Moderate zone. Activity in this zone helps improve aerobic fitness.)
+case 161...180:
+print(You are in the Hard zone. Activity in this zone increases maximum performance capacity for shorter sessions.)
+case 181...200: 
+print(You are in the Maximum zone. Activity in this zone helps fit athletes develop speed.) 
+}
+
  
  If `currentHR` is above the listed zones, print some kind of warning asking the user to slow down.
  */
 let currentHR = 128
+print(You need to slow down!)
 
 /*:
 [Previous](@previous)  |  page 7 of 9  |  [Next: Exercise - Ternary Operator](@next)

66c89f9ab46710a55e47659382a4da0d09ee07a9
hew6589 <wrheasian9189@hotmail.com>
2024-11-16 19:29:40 -0500

    Update Contents.swift

--- student_Xcode15/Unit2/Lesson2_2/Lab_Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_2/Lab_Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
@@ -9,14 +9,13 @@
  Create a `favoriteQuote` constant and assign it the following string literal:
  
 - "My favorite quote is <INSERT QUOTE HERE>."
-"My favorite quote is "Never stop dreaming."
+
  
  Write in your own favorite quote where indicated, and be sure to include escaped quotation marks. When finished, print the value of `favoriteQuote`.
 
  - Example: If your favorite quote is "The grass is always greener on the other side" the value of `favoriteQuote` should be such that printing `favoriteQuote` results in the following:
  * `My favorite quote is "The grass is always greener on the other side."`
- */
-
+ */"My favorite quote is "Never stop dreaming."
 
 //:  Write an if-else statement that prints "There's nothing here" if `emptyString` is empty, and "It's not as empty as I thought" otherwise.
 let emptyString = "There's nothing here"

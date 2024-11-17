31109ac85cab936967863ad9c59a571442c09b5d
hew6589 <wrheasian9189@hotmail.com>
2024-11-16 22:24:11 -0500

    Update Contents.swift
    Completed Lab 2.3

--- student_Xcode15/Unit2/Lesson2_3/Lab_Functions.playground/Pages/6. App Exercise - Separating Functions.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_3/Lab_Functions.playground/Pages/6. App Exercise - Separating Functions.xcplaygroundpage/Contents.swift
@@ -6,11 +6,19 @@
  One principle that can help in debugging and maintaining code is abstraction. For example, in your fitness tracking app some of your existing functions have been written to both perform a calculation and print a message. But it's very possible that you'll decide to change either the calculation or the message in the future. It will be easier to go back and change this if you separate the calculation from the message.
 
  As an example, write a function that only does a portion of what your previous `pacing` function did. This function will be called `calculatePace`. It should take three `Double` arguments called `currentDistance`, `totalDistance`, and `currentTime`, and should return a `Double` that will represent the time at which the user will finish the run based on the user's current distance and time. call the function and print the return value.
- */
-
+ */ func calculatePace
+let currentDistance = 2500 miles
+let totalDistance = 10000 miles
+let currentTime = 10:00
+return 1:20:00
 
 //:  Now write a function called `pacing` that takes four `Double` arguments called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. The function should also return a `String`, which will be the message to show the user. The function should call `calculatePace`, passing in the appropriate values, and capture the return value. The function should then compare the returned value to `goalTime` and if the user is on pace return "Keep it up!", and return "You've got to push it just a bit harder!" otherwise. Call the function and print the return value.
-
+func calculatePace 
+let currentDistance = 500 miles
+let totalDistance = 1000 miles
+let currentTime = 30:00
+let goalTime = 1:00:00
+return "Keep it up!"
 
 /*:
  _Copyright © 2023 Apple Inc._

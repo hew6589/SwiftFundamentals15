134eb8f812c4f6b457da343732019f5a45c12481
hew6589 <wrheasian9189@hotmail.com>
2024-11-16 20:52:49 -0500

    Update Contents.swift

--- student_Xcode15/Unit2/Lesson2_3/Lab_Functions.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_3/Lab_Functions.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
@@ -8,9 +8,21 @@
  A reoccurring process like this is a perfect candidate for a function. Write a function called `incrementSteps` after the declaration of `steps` below that will increment `steps` by one and then print its value. Call the function multiple times and observe the printouts.
  */
 var steps = 0
+func incrementSteps
+switch steps {
+ case 0...9:
+ print("Getting started")
+ case 10...99:
+ print("Almost halfway")
+ case 100...999:
+       print("Good Job")
+
+
 
 //:  Similarly, if you want to regularly provide progress updates to your user, you can put your control flow statements that check on progress into a function. Write a function called `progressUpdate` after the declaration of `goal` below. The function should print "You're off to a good start." if `steps` is less than 10% of `goal`, "You're almost halfway there!" if `steps` is less than half of `goal`, "You're over halfway there!" if `steps` is less than 90% of `goal`, "You're almost there!" if `steps` is less than `goal`, and "You beat your goal!" otherwise. Call the function and observe the printout.
 let goal = 10000 
+func progessUpdate
+print("You're almost there!")
 
 
 /*:

44790fa9a8bddd08268c799d1e914b6c3ac0a1dd
hew6589 <wrheasian9189@hotmail.com>
2024-11-16 19:05:53 -0500

    Update Contents.swift

--- student_Xcode15/Unit2/Lesson2_2/Lab_Strings.playground/Pages/4. Exercise - String Equality and Comparison.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_2/Lab_Strings.playground/Pages/4. Exercise - String Equality and Comparison.xcplaygroundpage/Contents.swift
@@ -2,14 +2,19 @@
 ## Exercise - String Equality and Comparison
  
  Create two constants, `nameInCaps` and `name`. Assign `nameInCaps` your name as a string literal with proper capitalization. Assign `name` your name as a string literal in all lowercase. Write an if-else statement that checks to see if `nameInCaps` and `name` are the same. If they are, print "The two strings are equal," otherwise print "The two strings are not equal."
- */
+ */let nameInCaps = "Wendy He"
+let name = "wendy he"
+if nameInCaps != name {
+ print("The two strings are not equal.") 
+}
 
 
 /*:
  Write a new if-else statement that also checks to see if `nameInCaps` and `name` are the same. However, this time use the `lowercased()` method on each constant to compare the lowercase version of the strings. If they are equal, print the following statement using string interpolations:
  
 - "<INSERT LOWERCASED VERSION OF `nameInCaps` HERE> and <INSERT LOWERCASED VERSION OF `name` HERE> are the same."
- 
+ "wendy he and wendy he are the same." 
+
  If they are not equal, print the following statement using string interpolation:
  
 - "<INSERT LOWERCASED VERSION OF `nameInCaps` HERE> and <INSERT LOWERCASED VERSION OF `name` HERE> are not the same."
@@ -18,6 +23,9 @@
 
 //:  Imagine you are looking through a list of names to find any that end in "Jr." Write an if statement below that will check if `junior` has the suffix "Jr." If it does, print "We found a second generation name!"
 let junior = "Cal Ripken Jr."
+if junior = Jr. {
+ print("We found a second generation name!")
+}
 
 
 
@@ -25,9 +33,14 @@ let junior = "Cal Ripken Jr."
 import Foundation
 let textToSearchThrough = "To be, or not to be--that is the question"
 let textToSearchFor = "to be, or not to be"
+if textToSearchThrough.contains("to be, or not to be") {
+ print("I found it!")
+}
 
 
 //:  Print to the console the number of characters in your name by using the `count` property on `name`.
+let name = "Wendy He"
+let count = name.count // 7
 
 
 /*:

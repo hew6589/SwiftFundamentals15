cd87aa6e5b59948b026f37a9a193064188dc0ccf
hew6589 <wrheasian9189@hotmail.com>
2024-11-17 21:47:44 -0500

    Update Contents.swift
    Completed Lab 2.4

--- student_Xcode15/Unit2/Lesson2_4/Lab_Structures.playground/Pages/10. App Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_4/Lab_Structures.playground/Pages/10. App Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
@@ -13,12 +13,15 @@ struct RunningWorkout {
     var distance: Double
     var time: Double
     var elevation: Double
+ static var mileTimeFor(distance:time:) = Double
+} 
 }
 
 
 
 //:  It may be helpful to have a few type properties on `RunningWorkout` representing unit conversions (i.e. meters to mile, feet to meters, etc.). Go back and add a type property for `meterInFeet` and assign it 3.28084. Then add a type property for `mileInMeters` and assign it 1600.0. Print both of these values below.
-
+static var meterInFeet = 3.28084
+static var mileInMeters = 1600.0
 
 /*:
  _Copyright © 2023 Apple Inc._

d101c67bb8ef7bbeae3098ad476a24304e79f25f
hew6589 <wrheasian9189@hotmail.com>
2024-11-24 17:57:16 -0500

    Update Contents.swift

--- student_Xcode15/Unit2/Lesson2_7/Lab_Loops.playground/Pages/1. Exercise - For-In Loops.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_7/Lab_Loops.playground/Pages/1. Exercise - For-In Loops.xcplaygroundpage/Contents.swift
@@ -2,15 +2,29 @@
 ## Exercise - For-In Loops
  
  Create a for-in loop that loops through values 1 to 100, and prints each of the values.
- */
+ */ for index in 1...100 {
+ print("This is number \(index)")
+}
 
 
 //:  Create a for-in loop that loops through each of the characters in the `alphabet` string below, and prints each of the values alongside the index.
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+for index in A...Z {
+print("This is letter \(index)")
+}
 
 
 //:  Create a `[String: String]` dictionary, where the keys are names of states and the values are their capitals. Include at least three key/value pairs in your collection, then use a for-in loop to iterate over the pairs and print out the keys and values in a sentence.
-
+["North Carolina": Raleigh, "Tennessee": Nashville, "South Carolina": Columbia] 
+for index in North Carolina {
+ print("This is capital \(Raleigh)")
+]
+for index in Tennessee {
+ print("This is capital \(Nashville)")
+}
+for index in South Carolina {
+ print("This is capital \(Columbia)")
+}
 
 /*:
 page 1 of 6  |  [Next: App Exercise - Movements](@next)

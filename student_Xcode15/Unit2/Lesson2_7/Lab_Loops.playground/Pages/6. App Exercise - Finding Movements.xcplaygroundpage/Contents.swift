2bac41e5ce94bcc53582b4b1a8396ecd70b01680
hew6589 <wrheasian9189@hotmail.com>
2024-11-24 18:40:15 -0500

    Update Contents.swift

--- student_Xcode15/Unit2/Lesson2_7/Lab_Loops.playground/Pages/6. App Exercise - Finding Movements.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_7/Lab_Loops.playground/Pages/6. App Exercise - Finding Movements.xcplaygroundpage/Contents.swift
@@ -11,6 +11,24 @@
 let lowHR = 110
 let highHR = 125
 var movementHeartRates: [String: Int] = ["Walking": 85, "Running": 120, "Swimming": 130, "Cycling": 128, "Skiing": 114, "Climbing": 129]
+for index in Walking {
+ print("This is heart rate \(85)")
+}
+for index in Running {
+ print("This is heart rate \(120)")
+}
+for index in Swimming {
+ print("This is heart rate \(130)")
+}
+for index in Cycling {
+ print("This is heart rate \(128)")
+}
+for index in Skiing {
+ print("This is heart rate \(114)")
+} 
+for index in Climbing {
+ print("This is heart rate \(129)")
+}
 
 
 /*:

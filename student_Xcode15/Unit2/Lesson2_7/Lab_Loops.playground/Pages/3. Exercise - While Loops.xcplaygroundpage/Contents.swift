c3fee1c0b31e2a5889db7f9ad08ed1f8a35d2727
hew6589 <wrheasian9189@hotmail.com>
2024-11-24 18:15:31 -0500

    Update Contents.swift

--- student_Xcode15/Unit2/Lesson2_7/Lab_Loops.playground/Pages/3. Exercise - While Loops.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_7/Lab_Loops.playground/Pages/3. Exercise - While Loops.xcplaygroundpage/Contents.swift
@@ -5,6 +5,7 @@ import Foundation
  Create a while loop that simulates rolling a 6-sided dice repeatedly until a 1 is rolled. After each roll, print the value. (Hint: use `Int.random(in: 1...6)` to generate a random number between 1 and 6).
  */
 var roll = 0
+while Int.random = 1
 
 
 /*:

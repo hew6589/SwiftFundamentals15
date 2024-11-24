356265268b30b3175e122a15f27a7a9974267ce7
hew6589 <wrheasian9189@hotmail.com>
2024-11-23 19:28:37 -0500

    Update Contents.swift

--- student_Xcode15/Unit2/Lesson2_5/Lab_Classes.playground/Pages/2. Exercise - Create a Subclass.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_5/Lab_Classes.playground/Pages/2. Exercise - Create a Subclass.xcplaygroundpage/Contents.swift
@@ -4,16 +4,16 @@
  - Note: The exercises below are based on a game where a spaceship avoids obstacles in space. The ship is positioned at the bottom of a coordinate system and can only move left and right while obstacles "fall" from top to bottom. Throughout the exercises, you'll create classes to represent different types of spaceships that can be used in the game. The base class `Spaceship` has been provided for you below.
  */
 class Spaceship {
-    var name: String = ""
+    var name: String = "Destroyer"
     var health = 100
-    var position = 0
+    var position = 10
     
     func moveLeft() {
         position -= 1
     }
     
     func moveRight() {
-        position += 1
+        position += 2
     }
     
     func wasHit() {
@@ -24,13 +24,23 @@ class Spaceship {
     }
 }
 //:  Define a new class `Fighter` that inherits from `Spaceship`. Add a variable property `weapon` that defaults to an empty string and a variable property `remainingFirePower` that defaults to 5.
+class Fighter
+var weapon = laser
+var remainingFirePower = 10
+
+func destroyer()
+func fire() {
+ power -= 1
+}
 
 
 //:  Create a new instance of `Fighter` called `destroyer`. A `Fighter` will be able to shoot incoming objects to avoid colliding with them. After initialization, set `weapon` to "Laser" and `remainingFirePower` to 10. Note that since `Fighter` inherits from `Spaceship`, it also has properties for `name`, `health`, and `position`, and has methods for `moveLeft()`, `moveRight()`, and `wasHit()` even though you did not specifically add them to the declaration of `Fighter`. Knowing that, set `name` to "Destroyer," print `position`, then call `moveRight()` and print `position` again.
 
 
 //:  Try to print `weapon` on `falcon`. Why doesn't this work? Provide your answer in a comment or a print statement below, and remove any code you added that doesn't compile.
-
+Class Falcon
+print("weapon")
+Because the weapon is not mention like a sword.
 
 //:  Add a method to `fighter` called `fire()`. This should check to see if `remainingFirePower` is greater than 0, and if so, should decrement `remainingFirePower` by one. If `remainingFirePower` is not greater than 0, print "You have no more fire power." Call `fire()` on `destroyer` a few times and print `remainingFirePower` after each method call.
 
